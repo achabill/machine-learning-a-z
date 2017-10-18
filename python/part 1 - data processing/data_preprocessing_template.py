59abd73c535fbecefff42d6b6695c42d6318eb8c
achabill <achabill12@gmail.com>
2017-10-18 16:58:39 +0100

    encoding categorical data

--- python/part 1 - data processing/data_preprocessing_template.py
+++ python/part 1 - data processing/data_preprocessing_template.py
@@ -15,3 +15,12 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:,1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+label_encoder_X = LabelEncoder()
+X[:,0] = label_encoder_X.fit_transform(X[:,0])
+one_hot_encoder = OneHotEncoder(categorical_features = [0])
+X = one_hot_encoder.fit_transform(X).toarray()
+label_encoder_y = LabelEncoder()
+y = label_encoder_y.fit_transform(y)
